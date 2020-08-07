2561aa5a04d6d22ef9d109104888dfb807a1bfbe
justicedong <justicedong@naver.com>
2020-08-07 19:38:57 +0900

    Add: UI 및 기본작업

--- src/App.js
+++ src/App.js
@@ -1,24 +1,53 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import './calculate.scss';
 
 function App() {
+  const [score, setScore] = useState(0);
+
+  const onClick = (value) => {
+    setScore(score => { if (score) { return score + value } return value; });
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="calc">
+        <div className="calc__title">
+          Calculator
+        </div>
+        <div className="calc__score">
+          {score}
+        </div>
+        <div className="calc__keyboard">
+          <div className="calc__keyboard__main">
+            <div className="button calc__keyboard__ac">AC</div>
+            <div classsName="calc__keyboard__number">
+              <div className="calc__keyboard__number__line">
+                <div className="button button--blue" onClick={() => onClick('1')}>1</div>
+                <div className="button button--blue" onClick={() => onClick('2')}>2</div>
+                <div className="button button--blue" onClick={() => onClick('3')}>3</div>
+              </div>
+              <div className="calc__keyboard__number__line">
+                <div className="button button--blue">4</div>
+                <div className="button button--blue">5</div>
+                <div className="button button--blue">6</div>
+              </div>
+              <div className="calc__keyboard__number__line">
+                <div className="button button--blue">7</div>
+                <div className="button button--blue">8</div>
+                <div className="button button--blue">9</div>
+              </div>
+            </div>
+            <div className="button button--blue--big">0</div>
+          </div>
+          <div className="calc__keyboard__operator">
+            <div className="button button--deepblue">+</div>
+            <div className="button button--deepblue">−</div>
+            <div className="button button--deepblue">×</div>
+            <div className="button button--deepblue">÷</div>
+            <div className="button button--pink">=</div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
